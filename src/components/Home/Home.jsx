789b26fefbe51a8a6b270731d93792e35c1d562b
JonathanPaita <jonathan.austria.paita@gmail.com>
2020-09-16 22:04:33 +0800

    added home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home_container'>
+        <img
+          className='home_image'
+          src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Projects/JumpIn/HongKong/JumpIn_HK_LANDINGPAGE_1500_2.jpg'
+          alt=''
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
